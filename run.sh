9007de878d58f6908a10686c7ba0fe91162ab13f
AndresMRM <andres@inventati.org>
2017-04-26 10:17:29 -0300

    gitlab

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 date
-pandoc -s -t revealjs apre.md -o index.html -V controls=false -V history=true -V transition=convex -A extra.html
-pandoc -s apre.md -o pagina.html
+pandoc -s -t revealjs apre.md -o docs/index.html -V controls=false -V history=true -V transition=convex -A extra.html
+pandoc -s apre.md -o docs/pagina.html
 ./reload-browser.sh Firefox
